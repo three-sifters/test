78a9aee20ba4c6f0e99e3efc14f55d94fc25e10f
Ryan Hodin <ryan951357@gmail.com>
2017-08-07 00:00:35 -0700

    Add image alt-text and title updates

--- scripts/galleries/gallery.js
+++ scripts/galleries/gallery.js
@@ -1,46 +1,6 @@
 // Gallery.js: Manages the dynamic operation of a gallery.
 
-var galleryIndex=1;
-
-// Fills in data on the current image
-function fillMetadata() {
-    var currentImage=gallery.images[galleryIndex];
-
-    document.getElementById("gallery-title").innerHTML=currentImage.name;
-    document.getElementById("gallery-description").innerHTML=currentImage.description;
-    if (currentImage.inStock==undefined)
-        currentImage.inStock=true;
-    document.getElementById("out-of-stock").style.display=currentImage.inStock ? "none" : "block";
-
-    var input=document.getElementById("gallery-units");
-    if (currentImage.minCount==undefined)
-        currentImage.minCount=1;
-    input.min=currentImage.minCount;
-    if (currentImage.defaultCount==undefined)
-        currentImage.defaultCount=1;
-    input.value=currentImage.defaultCount;
-    if (currentImage.maxCount==undefined)
-        input.removeAttribute("max");
-    else
-        input.max=currentImage.maxCount;
-    if (currentImage.stepCount==undefined)
-        currentImage.stepCount=1;
-    input.step=currentImage.stepCount;
-
-    if (currentImage.price==undefined)
-        currentImage.price=100;
-    var price=currentImage.price;
-    document.getElementById("gallery-dollars").innerHTML=Math.floor(price/100);
-    price=price%100;
-    var cents=price.toString();
-    if (cents.length<2)
-        cents="0"+cents;
-    document.getElementById("gallery-cents").innerHTML=cents;
-
-    input.disabled=!currentImage.inStock;
-    document.getElementById("gallery-order").disabled=input.disabled;
-}
-document.addEventListener("DOMContentLoaded", fillMetadata);
+var galleryIndex=4;
 
 var images=0;
 var pathPrefix="";
@@ -61,6 +21,62 @@ function prepImages() {
 
     pathPrefix=document.getElementById("gallery-path").value;
 }
+
+// Fills in data on the current image
+function fillMetadata() {
+    var currentImage=gallery.images[galleryIndex];
+
+    // Large and important items
+    document.getElementById("gallery-title").innerHTML=currentImage.name;
+    document.getElementById("gallery-description").innerHTML=currentImage.description;
+    if (currentImage.inStock==undefined)
+        currentImage.inStock=true;
+    document.getElementById("out-of-stock").style.display=currentImage.inStock ? "none" : "block";
+
+    // Input field
+    var input=document.getElementById("gallery-units");
+    if (currentImage.minCount==undefined)
+        currentImage.minCount=1;
+    input.min=currentImage.minCount;
+    if (currentImage.defaultCount==undefined)
+        currentImage.defaultCount=1;
+    input.value=currentImage.defaultCount;
+    if (currentImage.maxCount==undefined)
+        input.removeAttribute("max");
+    else
+        input.max=currentImage.maxCount;
+    if (currentImage.stepCount==undefined)
+        currentImage.stepCount=1;
+    input.step=currentImage.stepCount;
+
+    // Price
+    if (currentImage.price==undefined)
+        currentImage.price=100;
+    var price=currentImage.price;
+    document.getElementById("gallery-dollars").innerHTML=Math.floor(price/100);
+    price=price%100;
+    var cents=price.toString();
+    if (cents.length<2)
+        cents="0"+cents;
+    document.getElementById("gallery-cents").innerHTML=cents;
+
+    // Enable/disable inputs and add to cart depending on stock
+    input.disabled=!currentImage.inStock;
+    document.getElementById("gallery-order").disabled=input.disabled;
+
+    // Now, update image element alt-text and title
+    prepImages();
+    for (var i=0; i<images.length; ++i) {
+        var n=images[i].getAttribute("imagenumber");
+        if (gallery.images[n].alt==undefined)
+            gallery.images[n].alt=gallery.images[n].name;
+        images[i].alt=gallery.images[n].alt;
+        if (gallery.images[n].title==undefined)
+            gallery.images[n].title="";
+        images[i].title=gallery.images[n].title;
+    }
+}
+document.addEventListener("DOMContentLoaded", fillMetadata);
 
 function rotateGallery(offset) {
     if (offset==0) return; // Sanity check
