0ce55e6546bb1474080d6b61aa0a112a46acf8aa
Ryan Hodin <ryan951357@gmail.com>
2017-08-07 21:12:25 -0700

    Build the cart when the page is loaded

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,3 +1,48 @@
 // Manages behavior of the shopping cart
 
+var dirty=false; // Whether the cart needs saving
+var cartKey="shoppingCart"; // Constant, keep the same wherever the cart is referenced
 
+// Loads the shopping cart and fills in the appropriate items in the
+function structureCart() {
+    var storage=sessionStorage;
+    var cart=JSON.parse(storage.getItem(cartKey));
+    if (cart==null || cart.length==0) { // The cart does not exist or is empty.
+        document.getElementById("order-cart").disabled=true;
+        document.getElementById("cart-table").style.display="none";
+        document.getElementById("cart-empty").style.display="block";
+        return;
+    }
+
+    var clone;
+    var templateContainer=document.getElementById("cart-table-template-row-container")
+    var table=document.getElementById("cart-table-body");
+    table.removeChild(table.firstElementChild);
+    for (var i=0; i<cart.length; ++i) {
+        console.log(i);
+        for (var n=0; n<templateContainer.children.length; ++n) {
+            clone=templateContainer.children[n].cloneNode(true);
+            table.appendChild(clone);
+        }
+
+        var row=table.children[i];
+        row.getElementsByClassName("cart-item-image")[0].src=cart[i].product.image;
+        row.getElementsByClassName("cart-item-name")[0].innerHTML=cart[i].product.name;
+
+        var price=cart[i].product.cost;
+        row.getElementsByClassName("cart-item-collars")[0].innerHTML=Math.floor(price/100);
+        var cents=price%100;
+        cents=cents.toString();
+        if (cents.length<2)
+            cents="0"+cents;
+        row.getElementsByClassName("cart-item-cents")[0].innerHTML=cents;
+
+        var input=row.getElementsByClassName("cart-item-count")[0];
+        input.min=cart[i].product.min;
+        if (cart[i].product.max>0)
+            input.max=cart[i].product.max;
+        input.step=cart[i].product.step;
+        input.value=cart[i].count;
+    }
+}
+document.addEventListener("DOMContentLoaded", structureCart);
